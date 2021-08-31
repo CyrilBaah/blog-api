17ddd5871c750e34ead5e8bc7bca2a5dd426af75
CyrilBaah <cyril.baah@amalitech.org>
2021-08-31 16:09:07 +0000

    Signup

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,10 +1,36 @@
+const { User } = require('../../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signUp = async (req, res) => {
     try {
         const { username, email, image, password, confirmPassword } = req.body;
-        console.log(username, email, image, password, confirmPassword);
+        if (!(username && email && password && confirmPassword)) {
+            res.status(400).json({ success: false, message: `All fields are required` });
+            res.exit(0);
+        }
+
+        if (password !== confirmPassword) {
+            res.status(400).json({ success: false, message: `Password does't match` });
+            res.exit(0);
+        }
+
+        const user = await User.findOne({ where: { email } });
+        if (user) {
+            res.status(400).json({ success: false, message: `User already exist` });
+        } else {
+            encryptedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create({
+                username,
+                email,
+                image,
+                password: encryptedPassword,
+            });
+            res.status(201).json({ success:true, message: user });
+        }
+        res.status(400).json({ success: false, message: user });
     } catch (error) {
         console.log(error);
-        res.status(201).json({ success: false, message: error });
+        res.status(200).json({ success: false, message: error });
     }
 }
