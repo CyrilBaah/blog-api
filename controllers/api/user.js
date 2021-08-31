6ce869b6a53d5ab07e4bcd5c7df082ea9ce220b5
CyrilBaah <cyril.baah@amalitech.org>
2021-08-31 15:44:34 +0000

    Created User model

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,7 +1,8 @@
+
 exports.signUp = async (req, res) => {
     try {
-        const { username, email, password, confirmPassword } = req.body;
-        console.log(username, email, password, confirmPassword);
+        const { username, email, image, password, confirmPassword } = req.body;
+        console.log(username, email, image, password, confirmPassword);
     } catch (error) {
         console.log(error);
         res.status(201).json({ success: false, message: error });
