cc7ec5d848b9d514e2843314cd49220fbd86b996
CyrilBaah <cyril.baah@amalitech.org>
2021-09-01 15:03:09 +0000

    Authetication added

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/api/post');
+const auth = require('../helper/auth');
 
-router.post('/api/posts', postController.createPost);
-router.get('/api/posts', postController.getAllPost);
+router.post('/api/posts',auth, postController.createPost);
+router.get('/api/posts',auth, postController.getAllPost);
 
 module.exports = router;
