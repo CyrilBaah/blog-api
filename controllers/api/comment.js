32e616bd319c73756e40b2808e3f88870884c6f0
CyrilBaah <cyril.baah@amalitech.org>
2021-09-02 14:50:49 +0000

    Get all comments based on the PostUuid

--- controllers/api/comment.js
+++ controllers/api/comment.js
@@ -16,3 +16,29 @@ exports.createComment = async (req, res) => {
         res.status(400).json({ success: false, message: error });
     }
 }
+
+
+exports.getAllComments = async (req, res) => {
+    try {
+        const { uuid } = req.params;
+        const comments = await Comment.findAll({ where: { postUuid: uuid } });
+
+        res.status(201).json({ success: true, message: comments });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error });
+    }
+}
+
+
+exports.editComment = async (req, res) => {
+    try {
+        const { uuid } = req.params;
+        console.log(uuid);
+        // const comment = Comment.findOne({ where: })
+        // res.status(201).json({ success: true, message: comments });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error });
+    }
+}
--- routes/comment.js
+++ routes/comment.js
@@ -6,5 +6,11 @@ const auth = require('../helper/auth');
 // POST : /api/:postId/comment
 router.post('/api/:uuid/comments',auth, commentController.createComment);
 
+// GET : /api/:postId/comment
+router.get('/api/:uuid/comments',auth, commentController.getAllComments);
+
+
+// PUT : /api/:postId/comment/commentUuid
+router.put('/api/:uuid/comments/:uuid',auth, commentController.editComment); 
 
 module.exports = router;
