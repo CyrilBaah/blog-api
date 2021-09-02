fb9e6863af00148986c3ea8283766aad5cfe31f7
CyrilBaah <cyril.baah@amalitech.org>
2021-09-02 13:48:42 +0000

    Comment Model created

---
+++ migrations/20210902133754-create-comment.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      userUuid: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
---
+++ models/comment.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Comment.init({
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },  
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userUuid: {
+      type: DataTypes.UUIDV4,
+      allowNull: false
+    }
+    }, 
+    {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
