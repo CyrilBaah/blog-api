a18d702a6ec1d2cce932d20dd308c0dd3344d9be
CyrilBaah <cyril.baah@amalitech.org>
2021-09-07 15:33:30 +0000

    Swagger Set up

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const multer = require('multer');
 require('dotenv').config();
 const { sequelize } = require('./models');
 const { storage, fileFilter } = require('./helper/multer');
+const YAML = require('yamljs');
+const swaggerJsDocs = YAML.load('./api-docs/api.yaml');
+const swaggerUI = require('swagger-ui-express');
 
 
 app.use(express.json());
@@ -17,7 +20,7 @@ app.use('/public/uploads', express.static(__dirname + '/public/uploads'))
 app.get('/', (req, res) => {
     res.status(200).json({ success: true, message: "Welcome to Blog API" })
 });
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
